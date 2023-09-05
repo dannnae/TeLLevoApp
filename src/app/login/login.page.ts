6a07e95f474a96b64472a1a11a3a0d2d5e856238
dannnae <dan.venegas@duocuc.cl>
2023-09-05 18:08:57 -0300

    primer commit

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
+})
+export class LoginPage {
+  nombre!: string;
+  password!: string;
+  router!: Router;
+
+  constructor(_router: Router) {
+    this.router = _router;
+  }
+
+  login() {
+    if (this.password.length < 4) {
+      console.log('La contraseña debe tener al menos 4 caracteres');
+      return;
+    }
+    this.router.navigate(['/home'], { queryParams: { nombre: this.nombre } });
+  }
+
+}
---
+++ src/app/password/password.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password',
+  templateUrl: './password.page.html',
+  styleUrls: ['./password.page.scss'],
+})
+export class PasswordPage implements OnInit {
+  nombre!: string;
+  password1!: string;
+  password2!: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  guardar() {
+    let message = `Nombre: ${this.nombre} ${this.password1} ${this.password2}`;
+  }
+}
