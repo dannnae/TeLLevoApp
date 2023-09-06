f054724a00b8b414f2e90803d68247284a6838ae
dannnae <dan.venegas@duocuc.cl>
2023-09-06 17:42:45 -0300

    agregado nombre usuario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage implements OnInit {
+  nombre: string = '';
 
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.nombre = params['nombre'];
+    });
+  }
 }
