8361ee38bf1d8f157b71dcb9ea94b3bf1deb7f87
dannnae <dan.venegas@duocuc.cl>
2023-11-28 19:08:34 -0300

    endpoint viajes

--- src/app/services/django.service.ts
+++ src/app/services/django.service.ts
@@ -1,27 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/internal/Observable';
-import { retry } from 'rxjs/internal/operators/retry';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DjangoService {
-  apiURL = 'http://127.0.0.1:8000/api';
-  baseUrl: any;
-  
+  private apiURL = 'http://127.0.0.1:8000/api';
+  private apiKey = 'AIzaSyDbpBfS0Rcu1xJqPHgsUPAek-aJxYOU40g';
+
   constructor(private http: HttpClient) { }
 
-  login(email: string, password: string):Observable<any>{
-    return this.http.post(this.apiURL+'/login/', {
-      email, password
-    })
-    .pipe(retry(3));
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiURL}/login/`, { email, password })
+      .pipe(retry(3));
   }
 
-
-  register(userDetails: any) {
-    return this.http.post(`${this.apiURL}/usuarios/`, userDetails);
-    }
+  register(userDetails: any): Observable<any> {
+    return this.http.post(`${this.apiURL}/usuarios/`, userDetails)
+      .pipe(retry(3));
   }
+
+  crearViaje(viajeData: any): Observable<any> {
+    return this.http.post(`${this.apiURL}/viajes/`, viajeData)
+    
+  }
+}
+
+
 
