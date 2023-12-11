103d8f82e8b56fc82be24c62bbcc18d7414b2e91
dannnae <dan.venegas@duocuc.cl>
2023-12-11 15:33:34 -0300

    guard login > home

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@angular/core';
-import { 
-  ActivatedRouteSnapshot, 
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree } 
-  from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
+import { DjangoService } from '../services/django.service'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private router: Router, private djangoApi: DjangoService) {}
+
+  canActivate(): boolean {
+    const token = localStorage.getItem('token');
+    if (token) {
+      return true; 
+    } else {
+      this.router.navigate(['/login']);
+      return false; 
+    }
   }
-  
 }
